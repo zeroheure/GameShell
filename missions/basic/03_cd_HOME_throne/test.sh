a5b55493d598c1cef0cd53a6ce6d346035ec9854
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-07-01 17:34:12 +0200

    test

--- missions/basic/03_cd_HOME_throne/test.sh
+++ missions/basic/03_cd_HOME_throne/test.sh
@@ -8,22 +8,26 @@ add_cmd dummy2
 add_cmd dummy3
 
 cd
+add_cmd gsh check
 gsh assert check false
 
 add_cmd cd
 add_cmd cd "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')"
 cd
+add_cmd gsh check
 gsh assert check false
 
 add_cmd cd "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')"
 add_cmd cd ../../../
 add_cmd cd "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')"
 cd
+add_cmd gsh check
 gsh assert check false
 
 cd "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')"
 add_cmd cd
 add_cmd cd "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')"
+add_cmd gsh check
 gsh assert check true
 
 . alt_history_stop.sh
